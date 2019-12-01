1d0daa43da40d6416bf9c5cd7197d15bc6ef20e8
Dillon Ray Seals <dillonrayseals@gmail.com>
2019-11-30 21:21:05 -0600

    creating files

---
+++ backend.php
@@ -0,0 +1,14 @@
+<?php
+// access db connector
+include 'dbConnect.php';
+
+$conn = OpenCon();
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// set the MIME Type to application/json
+header("Content-Type: application/json");
+$json_str = file_get_contents('php://input');
+$json_obj = json_decode($json_str, true);
+?>
